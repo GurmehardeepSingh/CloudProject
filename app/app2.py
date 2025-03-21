d759620071792951353570852d7feb155c261559
GDS12359 <sgurmehardeep@gmail.com>
2025-03-21 10:26:24 +0530

    build test

--- app/app2.py
+++ app/app2.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template ,url_for
+from markupsafe import escape
 app=Flask(__name__)
 
 @app.route('/')
@@ -7,7 +8,14 @@ def index():
 
 @app.route('/name/<username>')
 def user(username):
-    return f"Hello {username}"
+    return f"Hello {escape (username)}"
+
+@app.route('/id')
+def id():
+    return f"Hello ID"
     
 if __name__ == "__main__":
     app.run(debug=True)
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('user', username='GDs'))
